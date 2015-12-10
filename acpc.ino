e8e2b76ea48ae2391ba67ca77bc0c8453011bca6
jake waters <jerkey@g-cipher.net>
2015-12-09 19:56:36 -0800

    LED animations work now

--- acpc.ino
+++ acpc.ino
@@ -1,13 +1,14 @@
 #define BAUDRATE 57600 // for serial communications
-#define VOLTMIN 80.0 // when relay connects outlets
+#define VOLTMIN 1.0 // when relay connects outlets
 #define VOLTMAX 140.0 // when relay disconnects outlets
 #define VOLTHYSTERESIS 5.0 // this many volts closer to ideal before relay closes
 #define RELAYMINTIME 1000 // minimum milliseconds relay must stay open before closing again
 
 #include <Adafruit_NeoPixel.h>
 
-#define STRIP1PIN 4 // pin number of LED strip
-#define NUMLEDS 65 // number of LEDs on strip
+#define STRIP1PIN 4 // pin number of LED strip 1
+#define STRIP1NUM 60 // number of LEDs on strip
+
 #define VOLTSPIN A0 // where to sense voltage
 #define AMPSPIN A1 // where to sense amperage
 #define RELAYPIN 2 // normally open relay for AC power
@@ -16,14 +17,34 @@
 #define AMPCOEFF 174.4 // 39 0.1A   252 at 1.28A    171 at 0.82A
 #define AMPOFFSET 28.0 // when nothing plugged in, this is the ADC value
 
-#define OVERSAMPLING 50 // how many times to average analogReads
+#define MAXPOWER 250.0 // wattage at which all LEDs are lit up
 
-Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(NUMLEDS, STRIP1PIN, NEO_GRB + NEO_KHZ800);
+#define OVERSAMPLING 25 // how many times to average analogReads
+
+Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(STRIP1NUM, STRIP1PIN, NEO_GRB + NEO_KHZ800);
 uint32_t offColor = 0; // the color used for pixels when "off"
+uint32_t onColor = 0; // the color used for pixels when "on"
 
 float ampsADC, voltsADC;
-float volts, amps;
+float volts, amps, watts;
 unsigned long lastRelayTime = 0; // remember the last time we flipped relay
+unsigned long lastLoopTime;
+int patternPos = 0; // for strip1 animation
+int fullness = 50;
+
+void animateLeds(float fullness) { // fullness is 0.0 to 1.0
+  unsigned long lastStrip1Time = millis();
+  int numLit = constrain(STRIP1NUM * fullness,0,STRIP1NUM-2);
+  for (int i = 0; i < STRIP1NUM; i++) {
+    if (i <= numLit) {
+    strip1.setPixelColor(STRIP1NUM-(i+patternPos)%STRIP1NUM-1,onColor);
+    } else {
+    strip1.setPixelColor(STRIP1NUM-(i+patternPos)%STRIP1NUM-1,offColor);
+    }
+  }
+  patternPos += 1;
+  strip1.show();
+}
 
 void updateRelay(float volts) {
   if (digitalRead(RELAYPIN)) { // if relay is closed
@@ -45,19 +66,18 @@ void setup() {
   pinMode(RELAYPIN,OUTPUT);
   strip1.begin();
   offColor = strip1.Color(10, 10, 10);
-  for (int i = 0; i < NUMLEDS; i++) strip1.setPixelColor(i,offColor);
+  onColor = strip1.Color(200, 200, 200);
+  for (int i = 0; i < STRIP1NUM; i++) strip1.setPixelColor(i,offColor);
   strip1.show();
-  lastRelayTime = millis();
+  lastLoopTime = millis();
 }
 
 void loop() {
-  for (int i = 0; i < NUMLEDS; i++) strip1.setPixelColor(i,strip1.Color(i,0,70-i));
-  strip1.show();
-
   voltsADC = averageRead(A0);
   volts = (voltsADC - VOLTOFFSET) / VOLTCOEFF;
   ampsADC = averageRead(A1);
   amps = (ampsADC - AMPOFFSET) / AMPCOEFF;
+  watts = amps * volts;
   Serial.print(volts,1);
   Serial.print("V (");
   Serial.print(voltsADC,1);
@@ -65,13 +85,17 @@ void loop() {
   Serial.print(amps,2);
   Serial.print("A (");
   Serial.print(ampsADC,1);
-  Serial.println(")  ");
-  if (Serial.available()) {
-    byte inByte = Serial.read();
-    if (inByte == 49) digitalWrite(RELAYPIN,HIGH);
-    if (inByte == 48) digitalWrite(RELAYPIN,LOW);
-    while (Serial.available()) inByte = Serial.read();
-  }
+  Serial.print(")  ");
+  Serial.print(watts,1);
+  Serial.print("W  ");
+  Serial.println(millis() - lastLoopTime);
+  lastLoopTime = millis();
+  /*if (Serial.available()) {
+    fullness = Serial.parseInt();
+    while (Serial.available()) byte inByte = Serial.read();
+  animateLeds((float)fullness / 100.0);
+  }*/
+  animateLeds(watts/MAXPOWER);
   updateRelay(volts);
 }
 
